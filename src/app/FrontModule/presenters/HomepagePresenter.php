f46c944ee81e131e8a61dd4066937c33745f3f63
zapotocnylubos <zapotocnylubos@gmail.com>
2019-02-28 13:14:21 +0100

    some changes only

--- src/app/FrontModule/presenters/HomepagePresenter.php
+++ src/app/FrontModule/presenters/HomepagePresenter.php
@@ -9,6 +9,7 @@ final class HomepagePresenter extends BasePresenter
 {
     public function actionDefault()
     {
+        // Redirect to built React directory
         $this->sendResponse(new RedirectResponse('/literature_set/'));
     }
 }
