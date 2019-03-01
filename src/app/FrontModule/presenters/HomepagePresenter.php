dc10e7e98adb859459f85660d627fe36a83dc69e
zapotocnylubos <zapotocnylubos@gmail.com>
2019-03-01 18:16:38 +0100

    redirect to form dynamically

--- src/app/FrontModule/presenters/HomepagePresenter.php
+++ src/app/FrontModule/presenters/HomepagePresenter.php
@@ -4,12 +4,13 @@ namespace App\FrontModule\Presenters;
 
 
 use Nette\Application\Responses\RedirectResponse;
+use Tracy\Debugger;
 
 final class HomepagePresenter extends BasePresenter
 {
     public function actionDefault()
     {
         // Redirect to built React directory
-        $this->sendResponse(new RedirectResponse('/formular/'));
+        $this->sendResponse(new RedirectResponse($this->getHttpRequest()->getUrl()->getHostUrl() . '/formular/'));
     }
 }
